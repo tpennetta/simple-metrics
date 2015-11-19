adb682beb6a872877bf37c90170e08f95f1ba848
Tom Penentta <tpennetta@gmail.com>
2015-11-19 15:27:44 -0500

    Initial Commit

---
+++ src/Metrics.js
@@ -0,0 +1,198 @@
+'use strict';
+/**
+ * Utility class for measuring elapsed time for performance testing. Performace
+ * is measured natively as microtime which can be converted to milliseconds or
+ * seconds. This is meant to be thin and minimal fo measuring execution times
+ * delegated by the user of this module.
+ * @module node-metrics
+ * @author Tom Pennetta <tpennetta@gmail.com>
+ * @example
+ * //Measure and track a particular metric
+ * var myMetrics = require('metric');
+ * var guid = myMetrics.start();
+ * //... perform tasks over time ...
+ * var elapsed = myMetrics.stop(guid);
+ * //elapsed now equals the time in microseconds of tasks performed.
+ */
+
+var uuid = require('uuid');
+var microtime = require('microtime');
+
+/**
+ * object collection of metric measurements with property keys of guids and
+ * property values of type object containing relevant information.
+ * @typedef {Object} Metrics~metrics
+ * @property {string} guid - Unique identifier key for metric measurement.
+ * @property {integer} guid.startTime - Initial time measurement in microseconds.
+ * @property {integer} guid.endTime - End time measurement in microseconds.
+ * @property {integer} guid.elapsedTime - Elapsed time measurement in microseconds.
+ * @private
+ */
+var metrics = {};
+
+/**
+ * create a new metric to add to metrics object. If passed no guid, a valid
+ * v4 uuid is generated.
+ * @param  {string}   guid     optional guid string to identify metric.
+ * @param  {Function} callback optional standard node callback function.
+ * @return {string}            guid identifier for current metric measurement.
+ */
+function start(guid, callback) {
+  var err = null;
+
+  if (arguments.length === 1 && typeof arguments[0] === 'function') {
+    guid = uuid.v4();
+    callback = arguments[0];
+  }
+
+  try {
+    guid = guid || uuid.v4();
+    metrics[guid] = {
+      startTime: microtime.now(),
+      endTime: null,
+      elapsedTime: null
+    };
+  } catch(ex) {
+    err = ex;
+  }
+
+  if (typeof callback === 'function') {
+    callback(err, guid);
+  } else {
+    if(err) throw err;
+    return guid;
+  }
+}
+
+/**
+ *
+ * @param  {[type]}   guid     [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+function stop(guid, callback) {
+  var err = null;
+  try {
+    isValidGuid(guid);
+    metrics[guid].endTime = microtime.now();
+    metrics[guid].elapsedTime = metrics[guid].endTime - metrics[guid].startTime;
+  } catch(ex) {
+    err = ex;
+  }
+
+  if (typeof callback === 'function') {
+    if(err) {
+      callback(err, null);
+    } else {
+      getElapsedTime(guid, callback);
+    }
+  } else {
+    if(err) throw err;
+    return getElapsedTime(guid);
+  }
+}
+
+/**
+ * return all the added/registered metric measurements.
+ * @param  {Function} callback optional node callback.
+ * @return {Object}            Object literal representation of all metrics.
+ */
+function getAllMetrics(callback) {
+  if (typeof callback === 'function') {
+    callback(null, metrics);
+  } else {
+    return metrics;
+  }
+}
+
+/**
+ * [getElapsedTime description]
+ * @param  {[type]}   guid     [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+function getElapsedTime(guid, callback) {
+  var err = null;
+
+  try {
+    isValidGuid(guid);
+  } catch(ex) {
+    err = ex;
+  }
+
+  if (typeof callback === 'function') {
+    callback(err, metrics[guid].elapsedTime);
+  } else {
+    if(err) throw err;
+    return metrics[guid].elapsedTime;;
+  }
+}
+
+function getAverage() {
+
+}
+
+function getMedian() {
+
+}
+
+/**
+ * [clear description]
+ * @param  {[type]}   guid     [description]
+ * @param  {Function} callback [description]
+ * @return {[type]}            [description]
+ */
+function clear(guid, callback) {
+  var err = null;
+
+  if (arguments.length === 1 && typeof arguments[0] === 'function') {
+    guid = null;
+    callback = arguments[0];
+  }
+
+  guid = guid || null;
+
+  try {
+    if (guid) {
+      isValidGuid(guid);
+      delete metrics[guid];
+    } else {
+      metrics = {};
+    }
+  } catch(ex) {
+    if(typeof callback === 'function') {
+      callback(ex, null);
+    } else {
+      throw ex;
+    }
+  }
+
+  if (typeof callback === 'function') {
+    callback(null, true);
+  } else {
+    return true;
+  }
+}
+
+/**
+ * [isValidGuid description]
+ * @param  {[type]}  guid [description]
+ * @return {Boolean}      [description]
+ */
+function isValidGuid(guid) {
+  if (!guid) {
+    throw new TypeError('GUID cannot be null or undefined');
+  }
+  if (!metrics[guid]) {
+    throw new Error('No metric registered for GUID: ' + guid);
+  }
+  return true;
+}
+
+module.exports = {
+  start: start,
+  stop: stop,
+  clear: clear,
+  getElapsedTime: getElapsedTime,
+  getAllMetrics: getAllMetrics
+};
---
+++ test/Metrics.spec.js
@@ -0,0 +1,117 @@
+var mocha = require('mocha'),
+    chai = require('chai'),
+    should = require('chai').should(),
+    uuid = require('uuid');
+
+var testMetrics = require('../src/Metrics.js')
+
+describe('Metrics', function() {
+  describe('Sync', function() {
+    var guid = null;
+    var elapsed = null;
+    it('should start a new metric measurement', function() {
+      guid = testMetrics.start();
+      guid.should.be.a('string');
+    });
+    it('should stop the current metric and record elapsed time', function() {
+      elapsed = testMetrics.stop(guid);
+      elapsed.should.be.a('number');
+    });
+    it('should add another new metric measurement', function() {
+      guid = null;
+      guid = testMetrics.start(uuid.v4());
+      guid.should.be.a('string');
+    });
+    it('should stop new metric measurement and record elapsed time', function() {
+      elapsed = testMetrics.stop(guid);
+      elapsed.should.be.a('number');
+    });
+    it('should get elapsed time by guid', function() {
+      testMetrics.getElapsedTime(guid).should.be.a('number');
+    });
+    it('should clear metric by guid', function() {
+      testMetrics.clear(guid);
+      (function() { testMetrics.getElapsedTime(guid); }).should.throw();
+    });
+    it('should clear all metrics', function() {
+      testMetrics.clear();
+      Object.keys(testMetrics.getAllMetrics()).length.should.equal(0);
+    });
+  });
+
+  describe('Async', function() {
+    var guid = null;
+    var elapsed = null;
+    it('should start a new metric measurement', function(done) {
+      testMetrics.start(function(err, res) {
+        if (err) {
+          throw err;
+        }
+        guid = res;
+        res.should.be.a('string');
+        done();
+      });
+    });
+    it('should stop the current metric and record elapsed time', function(done) {
+      testMetrics.stop(guid, function(err, res) {
+        if (err) {
+          throw err;
+        }
+        elapsed = res;
+        res.should.be.a('number');
+        done();
+      });
+    });
+    it('should add another new metric measurement', function(done) {
+      guid = null;
+      testMetrics.start(uuid.v4(), function(err, res) {
+        if(err) {
+          throw err;
+        }
+        guid = res;
+        res.should.be.a('string');
+        done();
+      });
+    });
+    it('should stop new metric measurement and record elapsed time', function(done) {
+      testMetrics.stop(guid, function(err, res) {
+        if(err) {
+          throw err;
+        }
+        elapsed = res;
+        res.should.be.a('number');
+        done();
+      });
+    });
+    it('should get elapsed time by guid', function(done) {
+      testMetrics.getElapsedTime(guid, function(err, res) {
+        if(err) {
+          throw err;
+        }
+        elapsed = res;
+        res.should.be.a('number');
+        done();
+      });
+    });
+    it('should clear metric by guid', function(done) {
+      testMetrics.clear(guid, function(err, res) {
+        if(err) {
+          throw err;
+        }
+        (function() { testMetrics.getElapsedTime(guid); }).should.throw();
+        res.should.equal(true);
+        done();
+      });
+    });
+    it('should clear all metrics', function(done) {
+      testMetrics.clear(function(err, res) {
+        if(err) {
+          throw err;
+        }
+        Object.keys(testMetrics.getAllMetrics()).length.should.equal(0);
+        res.should.equal(true);
+        done();
+      });
+    });
+  });
+});
